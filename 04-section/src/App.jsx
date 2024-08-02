baa27546a38028199450b32f469b508e96d09960
kimamovic21 <imamovic.kerim@gmail.com>
2024-08-02 17:14:20 +0200

    Section 04: A Better Development Environment
    -kreirani startni folderi sa fajlovima: 00-simple-tailwind-starter i 00-webpack-tailwind-starter
    -kreiran section.txt fajl
    -manje izmjene u documentation.txt fajlu

---
+++ 04-section/src/App.jsx
@@ -0,0 +1,19 @@
+const App = () => {
+  return (
+    <div className="m-2">
+      <h1>Simple Tailwind Starter</h1>
+      <h2>Hello World</h2>
+      <button className="btn-blue">Click me</button>
+      <div className="content-area">
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad repellat, sequi, iure animi sed officia 
+          quas adipisci modi ab qui alias totam non cupiditate minus placeat molestiae suscipit perspiciatis, 
+          dignissimos atque eius tenetur quod commodi? Assumenda mollitia amet vel maxime natus quo aliquid ullam, 
+          quidem corporis. Eos harum necessitatibus nisi.
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ 04-section/tailwind.config.js
@@ -0,0 +1,51 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: 'class',
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    fontFamily: {
+      'sans': ['ui-sans-serif', 'system-ui', 'Graphik'],
+      'serif': ['ui-serif', 'Georgia', 'Tapestry', 'Merriweather'],
+      'mono': ['ui-monospace', 'SFMono-Regular'],
+      'display': ['Oswald'],
+      'body': ['Open Sans'],
+    },
+    extend: {
+      colors: {
+        primary: '#ff5733',
+        secondary: '#fffc33',
+      },
+      spacing: {
+        6: '2rem',
+        24: '10rem',
+        128: '32rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      },
+      screens: {
+        sm: '550px',
+        md: '800px',
+        lg: '1200px',
+        xl: '1440px',
+      },
+      transitionDuration: {
+        1000: '1000ms',
+      },
+      animation: {
+        'spin-slow': 'spin 3s linear infinite',
+        wiggle: 'wiggle 1s ease-in-out infinite'
+      },
+      keyframes: {
+        wiggle: {
+          '0%, 100%': { transform: 'rotate(-12deg)' },
+          '50%': { transform: 'rotate(12deg)' }
+        }
+      },
+    },
+  },
+  plugins: [],
+}
